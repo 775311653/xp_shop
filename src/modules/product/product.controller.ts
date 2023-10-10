a52b00cec7d781b55ee094b75111a3dde0a233a2
xieping <775311653@qq.com>
2023-10-10 08:55:54 +0800

    1、detail modify

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -11,7 +11,7 @@ import {
   Post,
   Query
 } from "@nestjs/common";
-import {ApiOperation, ApiTags} from "@nestjs/swagger";
+import {ApiOkResponse, ApiOperation, ApiTags} from "@nestjs/swagger";
 import {ProductService} from "@src/modules/Product/product.service";
 import {ProductDTO} from "@src/modules/Product/dto/product.dto";
 import {ProductListVo, ProductVo} from "@src/modules/product/product.vo";
@@ -24,7 +24,7 @@ export class ProductController {
   }
 
   @Post()
-  @ApiOperation({ summary: '创建商品', description: '创建商品' })
+  @ApiOperation({summary: '创建商品', description: '创建商品'})
   @HttpCode(HttpStatus.CREATED)
   async createProduct(@Body() createProductDto: ProductDTO): Promise<string> {
     return await this.productService.createProduct(createProductDto);
@@ -32,15 +32,23 @@ export class ProductController {
 
 
   @Get('list')
-  @ApiOperation({ summary: '获取商品列表', description: '获取商品列表' })
+  @ApiOperation({summary: '获取商品列表', description: '获取商品列表'})
+  @ApiOkResponse({
+    type: ProductListVo,
+    description: '获取商品列表返回值',
+  })
   @HttpCode(HttpStatus.OK)
-  async getProductList( @Query() productReqDto: ProductReqDto ): Promise<ProductListVo> {
+  async getProductList(@Query() productReqDto: ProductReqDto): Promise<ProductListVo> {
     return await this.productService.getProductList(productReqDto);
   }
 
 //  写出剩下的删除、修改、查询的接口
   @Get(':id')
-  @ApiOperation({ summary: '获取商品', description: '根据商品ID获取商品' })
+  @ApiOperation({summary: '获取商品', description: '根据商品ID获取商品'})
+  @ApiOkResponse({
+    type: ProductVo,
+    description: '获取商品返回值',
+  })
   @HttpCode(HttpStatus.OK)
   async getProduct(
     @Param('id', new ParseIntPipe()) id: number,
@@ -49,16 +57,20 @@ export class ProductController {
   }
 
   @Patch(':id')
-  @ApiOperation({ summary: '修改商品', description: '根据商品ID修改商品' })
+  @ApiOperation({summary: '修改商品', description: '根据商品ID修改商品'})
+  @ApiOkResponse({
+    type: ProductVo,
+    description: '修改商品返回值',
+  })
   @HttpCode(HttpStatus.OK)
-  async modifyProductById( @Param('id', new ParseIntPipe()) id: number, @Body() productDTO: ProductDTO ): Promise<ProductVo> {
+  async modifyProductById(@Param('id', new ParseIntPipe()) id: number, @Body() productDTO: ProductDTO): Promise<ProductVo> {
     return await this.productService.modifyProductById(id, productDTO);
   }
 
   @Delete(':id')
-  @ApiOperation({ summary: '删除商品', description: '根据商品ID删除商品' })
+  @ApiOperation({summary: '删除商品', description: '根据商品ID删除商品'})
   @HttpCode(HttpStatus.OK)
-  async destroyProductById( @Param('id', new ParseIntPipe()) id: number ): Promise<string> {
+  async destroyProductById(@Param('id', new ParseIntPipe()) id: number): Promise<string> {
     return await this.productService.destroyProductById(id);
   }
 }
